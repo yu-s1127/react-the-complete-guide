8c29a7ec58e5bf6c64d5dfe85a67433f83a1b3f5
yuhei_suzuki <yooopopo5@gmail.com>
2022-01-07 02:12:59 +0900

    不要なインポートの削除

--- src/components/Users/AddUser.tsx
+++ src/components/Users/AddUser.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, FormEvent, useState, useRef } from 'react';
+import React, { FC, FormEvent, useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
